b035ebbf23820887a118e2e4d7957fb06f3d83a5
EmeraldNV <abdellah316619@gmail.com>
2026-01-11 23:55:18 +0100

    Add letter animation on page load with random opacity toggle

--- script/script.js
+++ script/script.js
@@ -3,6 +3,8 @@ const getStarted = document.querySelector(".button");
 const viewPortHeightElement = document.querySelector(".mainHeader");
 let statusViewPortHeight = false;
 
+const letters = document.querySelectorAll("g>g");
+console.log(letters);
 const scroll = () => {
   let viewPortHeight = viewPortHeightElement.offsetHeight;
 
@@ -21,9 +23,14 @@ const scroll = () => {
   }
 };
 
+const animateLetters = () => {
 
+  const randomIndex = Math.floor(Math.random() * letters.length);
+  const singleLetter = letters[randomIndex];
+   const currentOpacity = singleLetter.getAttribute("opacity");
+    singleLetter.setAttribute("opacity", currentOpacity === "0" ? "1" : "0");
+};
 
-
-
-window.onscroll = scroll;
-
+window.addEventListener("load", () => {
+  setInterval(animateLetters, 50);
+});
